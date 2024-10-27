6fe8d7a7c3e2fcbc2c0c52ab0a1089e4f3b3dd34
Kolin Zhao & Mozhimen <58215586+mozhimen@users.noreply.github.com>
2024-10-27 17:12:14 +0800

    1027

--- videok_nodemedia_test/src/main/java/com/mozhimen/videok/nodemedia/test/RTSPMainActivity.kt
+++ videok_nodemedia_test/src/main/java/com/mozhimen/videok/nodemedia/test/RTSPMainActivity.kt
@@ -1,12 +1,14 @@
 package com.mozhimen.videok.nodemedia.test
 
 import android.os.Bundle
+import android.util.Log
 import cn.nodemedia.NodePlayer
 import com.mozhimen.bindk.bases.viewdatabinding.activity.BaseActivityVDB
 import com.mozhimen.videok.nodemedia.test.databinding.ActivityMainBinding
 
 class RTSPMainActivity : BaseActivityVDB<ActivityMainBinding>() {
-    private val _rtspUrl = "rtsp://admin:shangqu2020@192.168.2.26:554/cam/realmonitor?channel=1&subtype=0"
+    private val _rtspUrl = "rtsp://admin:shangqu2020@192.168.2.26:554/cam/realmonitor?channel=1&subtype=0"//dahua
+    private val _rtspUrl1 = "rtsp://admin:qwer2580@192.168.2.200:554/Streaming/Channels/1"//hik
     private var _nodePlayer: NodePlayer? = null
 
     override fun initData(savedInstanceState: Bundle?) {
@@ -14,10 +16,13 @@ class RTSPMainActivity : BaseActivityVDB<ActivityMainBinding>() {
         _nodePlayer!!.apply {
             setPlayerView(vdb.mainRtspView)        //设置RTSP流使用的传输协议,支持的模式有:
             setRtspTransport(NodePlayer.RTSP_TRANSPORT_TCP)
-            setInputUrl(_rtspUrl)        //设置视频是否开启
+            setInputUrl(_rtspUrl1)        //设置视频是否开启
             setVideoEnable(true)
             setBufferTime(500)
             setMaxBufferTime(1000)
+            setNodePlayerDelegate { player, event, msg ->
+                Log.d(TAG, "initData: event $event msg $msg")
+            }
         }
         super.initData(savedInstanceState)
     }
